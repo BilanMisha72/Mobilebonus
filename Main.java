b1f435bd360f9030ad5487b2195ea2941696c74d
Misha <bilan.md72@gmail.com>
2023-04-06 22:32:48 +0500

    program

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 200; //остаток
+        int b = 1300; //платеж
+
+
+
+        int c = (a + b);
+        System.out.println(c);
+    }
+}
+
+
